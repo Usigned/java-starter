89c825cb15c2562b642b811894bf9399e86dfde9
Qing <843710843@qq.com>
2021-05-17 21:01:42 +0800

    fix warinings

--- src/io/CopyCharacters.java
+++ src/io/CopyCharacters.java
@@ -1,7 +1,7 @@
 package io;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
+// import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
