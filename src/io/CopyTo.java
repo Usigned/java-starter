a173fe25a6cd5a91db7b013f3d9efc71af7747c4
Qing <843710843@qq.com>
2021-12-23 20:54:32 +0800

    2021-09-14

--- src/io/CopyTo.java
+++ src/io/CopyTo.java
@@ -1,10 +1,12 @@
 package io;
 
-import java.io.*;
-import java.nio.file.*;
-import java.sql.Time;
-import java.util.Timer;
-import java.util.stream.Stream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 public class CopyTo {
     public static void copyTo(Path src, Path dist) {
